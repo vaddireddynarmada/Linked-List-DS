9d74eec4b2a027b1a19cb95b518f188ab001bce7
unknown <narmada.vaddireddy97@gmail.com>
2023-03-09 22:06:51 +0530

    Added: Code to print the Linked List append

--- LinkedList.java
+++ LinkedList.java
@@ -1,23 +1,45 @@
 public class LinkedList {
     private INode head;
     private INode tail;
+
     public LinkedList() {
+
         this.head = null;
         this.tail = null;
     }
+
     public void add(INode newNode) {
-        if (this.tail == null) {
+        if(this.tail == null) {
             this.tail = newNode;
         }
-        if (this.head == null) {
+        if(this.head == null) {
             this.head = newNode;
-        } else {
-            INode tempNode = this.head;
+        }
+        else {
+            INode temporarayNode = this.head;
             this.head = newNode;
-            this.head.setNext(tempNode);
+            this.head.setNext(temporarayNode);
+
+        }
+
+    }
+
+    public void append (INode newNode) {
+        if(this.tail == null) {
+            this.tail = newNode;
+        }
+        if(this.head == null) {
+            this.head = newNode;
+        }
+        else {
+            INode tempINode = this.tail;
+            this.tail.setNext(newNode);
+            this.tail = newNode;
         }
     }
-    public void print() {
-        System.out.println("My Nodes: " + head);
+
+    public void print () {
+        System.out.println("My Nodes: "+head);
     }
+
 }
--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,12 +1,13 @@
+
 public class LinkedListMain {
     public static void main(String[] args) {
-        MyNode<Integer> firstNode = new MyNode<Integer>(70);
-        MyNode<Integer> secondNode = new MyNode<Integer>(30);
-        MyNode<Integer> thirdNode = new MyNode<Integer>(56);
-        LinkedList myLinkedList = new LinkedList();
-        myLinkedList.add(firstNode);
-        myLinkedList.add(secondNode);
-        myLinkedList.add(thirdNode);
-        myLinkedList.print();
+        MyNode<Integer> Node1 = new MyNode<Integer>(56);
+        MyNode<Integer> Node2 = new MyNode<Integer>(30);
+        MyNode<Integer> Node3 = new MyNode<Integer>(70);
+        LinkedList myLinkedList1 = new LinkedList();
+        myLinkedList1.append(Node1);
+        myLinkedList1.append(Node2);
+        myLinkedList1.append(Node3);
+        myLinkedList1.print();
     }
 }
